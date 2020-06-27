b104b72541d211feb685541d214330b2cb0cd46a
Carlos Solorzano <csolorzanoflores@gmail.com>
2020-06-27 14:51:41 -0600

    Negative number failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -12,8 +12,17 @@ namespace Kata
             {
                 return 0;                
             }
+
+            var separator = new string[]{",", "\n"};
+            var input = number;
+            if (number.StartsWith("//"))
+            {
+                var parts = number.Split("\n");
+                input = parts[1];
+                separator = new string[]{parts[0].Replace("//","")};
+            }
             
-            var numbers = number.Split(new string[]{",", "\n"}, StringSplitOptions.None).Select(int.Parse);
+            var numbers = input.Split(separator, StringSplitOptions.None).Select(int.Parse);
             return numbers.Sum();
         }
     }
